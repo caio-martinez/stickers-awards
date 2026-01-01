605d0ad4625e553d26d138eb9111dec6775bb706
Caio Martinez <33815674+caio-martinez@users.noreply.github.com>
2026-01-01 11:52:35 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -4,50 +4,76 @@
 
 const categoriesConfig = [
     {
-        name: "Melhor Figurinha Engraçada",
+        name: "Figurinha 'Ai que Ódio'",
         nominees: [
-            {
-                caption: "gato rindo",
-                path: "categories/Melhor Figurinha Engraçada/gato rindo.png"
-            },
-            {
-                caption: "stonks meme",
-                path: "categories/Melhor Figurinha Engraçada/stonks meme.png"
-            },
-            {
-                caption: "facepalm",
-                path: "categories/Melhor Figurinha Engraçada/facepalm.png"
-            }
+            { caption: "Faz o L (censurado)", path: "categories/Figurinha 'Ai que Ódio'/Faz o L (censurado).gif" },
+            { caption: "Gritito", path: "categories/Figurinha 'Ai que Ódio'/Gritito.jpg" },
+            { caption: "Mulher mordendo laptop", path: "categories/Figurinha 'Ai que Ódio'/Mulher mordendo laptop.jpg" },
+            { caption: "não eh sobre isso e ta tudo mal", path: "categories/Figurinha 'Ai que Ódio'/não eh sobre isso e ta tudo mal.jpg" },
+            { caption: "tem que ir né fia, é contrato", path: "categories/Figurinha 'Ai que Ódio'/tem que ir né fia, é contrato.jpg" },
+            { caption: "temperatura palhaço", path: "categories/Figurinha 'Ai que Ódio'/temperatura palhaço.png" }
         ]
     },
     {
-        name: "Figurinha Mais Fofa",
+        name: "Figurinha Gretchen",
         nominees: [
-            {
-                caption: "gatinho dormindo",
-                path: "categories/Figurinha Mais Fofa/gatinho dormindo.png"
-            },
-            {
-                caption: "coração nos olhos",
-                path: "categories/Figurinha Mais Fofa/coração nos olhos.png"
-            },
-            {
-                caption: "troféu dourado",
-                path: "categories/Figurinha Mais Fofa/troféu dourado.png"
-            }
+            { caption: "gretchen brava", path: "categories/Figurinha Gretchen/gretchen brava.gif" },
+            { caption: "gretchen devastada", path: "categories/Figurinha Gretchen/gretchen devastada.png" },
+            { caption: "gretchen escrevendo", path: "categories/Figurinha Gretchen/gretchen escrevendo.gif" },
+            { caption: "gretchen faca", path: "categories/Figurinha Gretchen/gretchen faca.gif" },
+            { caption: "gretchen livreee", path: "categories/Figurinha Gretchen/gretchen livreee.gif" },
+            { caption: "gretchen sambando", path: "categories/Figurinha Gretchen/gretchen sambando.gif" }
         ]
     },
     {
-        name: "Melhor Reação",
+        name: "Figurinha Mais Coitadolândia",
         nominees: [
-            {
-                caption: "facepalm reação",
-                path: "categories/Melhor Reação/facepalm reação.png"
-            },
-            {
-                caption: "apaixonado",
-                path: "categories/Melhor Reação/apaixonado.png"
-            }
+            { caption: "Gatinho Tadinho", path: "categories/Figurinha Mais Coitadolândia/Gatinho Tadinho.jpg" },
+            { caption: "cachorro abraçando móvel", path: "categories/Figurinha Mais Coitadolândia/cachorro abraçando móvel.png" },
+            { caption: "doguinho triste olhando pra baixo", path: "categories/Figurinha Mais Coitadolândia/doguinho triste olhando pra baixo.png" },
+            { caption: "gatinho acabado", path: "categories/Figurinha Mais Coitadolândia/gatinho acabado.png" },
+            { caption: "gatinho quero dinheiro infinito", path: "categories/Figurinha Mais Coitadolândia/gatinho quero dinheiro infinito.png" },
+            { caption: "gato chorando", path: "categories/Figurinha Mais Coitadolândia/gato chorando.webp" },
+            { caption: "gato molhado", path: "categories/Figurinha Mais Coitadolândia/gato molhado.jpg" },
+            { caption: "infeliz no simples", path: "categories/Figurinha Mais Coitadolândia/infeliz no simples.png" }
+        ]
+    },
+    {
+        name: "Figurinha Mais Malandragem",
+        nominees: [
+            { caption: "duas loiras misteriosas", path: "categories/Figurinha Mais Malandragem/duas loiras misteriosas.png" },
+            { caption: "flor malandra cuphead", path: "categories/Figurinha Mais Malandragem/flor malandra cuphead.png" },
+            { caption: "inês brasil de boné", path: "categories/Figurinha Mais Malandragem/inês brasil de boné.png" },
+            { caption: "mulher bandida colômbia", path: "categories/Figurinha Mais Malandragem/mulher bandida colômbia.png" },
+            { caption: "urias passando prancha", path: "categories/Figurinha Mais Malandragem/urias passando prancha.png" }
+        ]
+    },
+    {
+        name: "Figurinha com Som",
+        nominees: [
+            { caption: "Heloisa Vogue", path: "categories/Figurinha com Som/Heloisa Vogue.gif" },
+            { caption: "a morena ta viva", path: "categories/Figurinha com Som/a morena ta viva.gif" },
+            { caption: "bugouu", path: "categories/Figurinha com Som/bugouu.gif" },
+            { caption: "nossa bicho que odio", path: "categories/Figurinha com Som/nossa bicho que odio.gif" },
+            { caption: "éééé Gata", path: "categories/Figurinha com Som/éééé Gata.png" }
+        ]
+    },
+    {
+        name: "Figurinha que serviu mais CUNT",
+        nominees: [
+            { caption: "Bart - Teck Teck", path: "categories/Figurinha que serviu mais CUNT/Bart - Teck Teck.jpg" },
+            { caption: "Doguinho de óculos rosa", path: "categories/Figurinha que serviu mais CUNT/Doguinho de óculos rosa.png" },
+            { caption: "cachorro óculos escuro malandro", path: "categories/Figurinha que serviu mais CUNT/cachorro óculos escuro malandro.png" },
+            { caption: "prontah", path: "categories/Figurinha que serviu mais CUNT/prontah.png" }
+        ]
+    },
+    {
+        name: "Figurinha Última Antes de Dormir",
+        nominees: [
+            { caption: "gatinho barriga pra cima", path: "categories/Figurinha Última Antes de Dormir/gatinho barriga pra cima.png" },
+            { caption: "gatos dormindo", path: "categories/Figurinha Última Antes de Dormir/gatos dormindo.png" },
+            { caption: "gatos dormindo 2", path: "categories/Figurinha Última Antes de Dormir/gatos dormindo 2.png" },
+            { caption: "mo pq vc viro", path: "categories/Figurinha Última Antes de Dormir/mo pq vc viro.png" }
         ]
     }
 ];
